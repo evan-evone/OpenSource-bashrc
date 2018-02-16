a8ce028feea36d02e680bc59d5ebb5021dcda715
Evan Keeton <2044295@edison.k12.nj.us>
2018-02-16 11:14:59 -0500

    Rearranged `fetch.sh` to output to `.ping_output` in repo, not arbitrary directory.

--- fetch.sh
+++ fetch.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+cd $1
+
 ping -q -c 1 github.com 2>.ping_output 1>.ping_output
 
 if [ "$?" == "0" ]; then
-  cd $1
   git fetch
   if [ "$(git df origin/master)" ]
     then echo "git: .bashrc differs"
   fi
-  cd $HOME
 fi
+
+cd $HOME
