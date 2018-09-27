141f4c21be8fc111fe6dcfed652ef5cf8ec78a09
Evan Keeton <keetonnj@edison.k12.nj.us>
2018-09-27 07:25:59 -0400

    Add fallback command for if PING works but GitHub is blocked

--- fetch.sh
+++ fetch.sh
@@ -5,9 +5,13 @@ cd $1
 ping -q -c 1 -t 2 github.com 2>/dev/null 1>/dev/null
 
 if [ "$?" == "0" ]; then
-  git fetch
-  if [ "$(git diff origin/$(git rev-parse --abbrev-ref HEAD))" ]
-    then echo "git: .bashrc differs"
+  git fetch 2>/dev/null 1>/dev/null
+  if [ "$?" == "0" ]; then
+    if [ "$(git diff origin/$(git rev-parse --abbrev-ref HEAD))" ]
+      then echo "git: .bashrc differs"
+    fi
+  else
+    echo "GitHub unavailable"
   fi
 else
   echo "PING failure"
