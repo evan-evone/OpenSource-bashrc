529f9fe5fc92e269f7984d31ac1a54011e18e25e
Evan Keeton <keetonnj@edison.k12.nj.us>
2018-10-31 17:29:07 -0400

    Change use of `myvim` to `.vimrc`
    - Much cleaner, more standard, and much more flexible.
    - Remove `myvim` function
    - Remove old `.vimdefault` file
    - Add `.vimrc`
    - Add `.vimrc` to install script

---
+++ .vimrc
@@ -0,0 +1,25 @@
+" ---
+" 'Reset' button for vim
+
+set nocompatible
+
+" ---
+" Setting up tabbing, indenting, etc.
+
+filetype indent plugin on
+syntax on
+set autoindent
+set backspace=indent,eol,start
+
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+set tw=92
+
+" ---
+" UI Options
+
+set hidden
+set wildmenu
+set showcmd
+set hlsearch
--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 if [ -f $HOME/.bash_profile ]; then rm $HOME/.bash_profile; fi
 if [ -f $HOME/.bashrc ]; then rm $HOME/.bashrc; fi
 if [ -f $HOME/.hyper.js ]; then rm $HOME/.hyper.js; fi
+if [ -f $HOME/.vimrc ]; then rm $HOME/.vimrc; fi
 ln -s $(pwd)/.bash_profile $HOME
 ln -s $(pwd)/.bashrc $HOME
 ln -s $(pwd)/.hyper.js $HOME
+ln -s $(pwd)/.vimrc $HOME
